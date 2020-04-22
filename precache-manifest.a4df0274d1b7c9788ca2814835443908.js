bf70d90efe58ffb9971bd2490de624e69aa78c9a
thabisoddx <thabisomoha@gmail.com>
2020-04-22 18:29:54 +0200

    Updates

---
+++ precache-manifest.a4df0274d1b7c9788ca2814835443908.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffe2766063eaadcf62d2abdabfd1441",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "3822ebcccb94eb2562cb",
+    "url": "/covid-dashboard/static/css/main.2a83f95d.chunk.css"
+  },
+  {
+    "revision": "8d1bec4da80424bb34ae",
+    "url": "/covid-dashboard/static/js/2.edda0df4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-dashboard/static/js/2.edda0df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3822ebcccb94eb2562cb",
+    "url": "/covid-dashboard/static/js/main.703dd467.chunk.js"
+  },
+  {
+    "revision": "1746489ac713bd07b55a",
+    "url": "/covid-dashboard/static/js/runtime-main.60ead92d.js"
+  },
+  {
+    "revision": "2984fce61c1950b3be51102baff553b6",
+    "url": "/covid-dashboard/static/media/ambulance.2984fce6.svg"
+  },
+  {
+    "revision": "e785e63a052d95d6ef5d0aaa294f648c",
+    "url": "/covid-dashboard/static/media/doctor.e785e63a.svg"
+  },
+  {
+    "revision": "1edafbf45e30607e3d15f036fb73baa5",
+    "url": "/covid-dashboard/static/media/healthcare-and-medical.1edafbf4.svg"
+  },
+  {
+    "revision": "83af99e579bfd8cdde1aa27b912fcebd",
+    "url": "/covid-dashboard/static/media/heart-beat.83af99e5.svg"
+  },
+  {
+    "revision": "0d004de31ebd86ceddc92c07862baa2e",
+    "url": "/covid-dashboard/static/media/shield.0d004de3.svg"
+  }
+]);
